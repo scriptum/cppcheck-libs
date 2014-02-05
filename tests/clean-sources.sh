5062e66bd0b35ca7da32d4b40f54f8da9ca169e8
Pavel Roschin <rpg@scriptumplus.ru>
2014-02-05 19:26:33 +0400

    add more tests

--- tests/clean-sources.sh
+++ tests/clean-sources.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find gtk selinux std -not -name "*.[ch]" $@
+find gtk selinux std full -not -name "*.[ch]" -and -not -name "*.cpp" -and -not -name "*.hpp" -and -not -name "*.cc" -and -not -name "*.hh" $@
