c53f48f2cdaf849bdd6e5ade964ed81bf9fb52df
Pavel Roschin <roshin@scriptumplus.ru>
2014-02-09 00:07:07 +0400

    g_list_sort just returns first pointer (maybe changed) so skip it

--- gtk.h
+++ gtk.h
@@ -3,3 +3,6 @@
 #define g_return_val_if_fail(expr, val) do{if(!(expr)){return val;}}while(0)
 #define g_return_if_reached() do{return;}while(0)
 #define g_return_val_if_reached(val) do{return val;}while(0)
+/* replace g_list_sort coz it just returns first pointer */
+#define g_list_sort(a, b) a
+#define g_list_sort_with_data(a, b, c) a
