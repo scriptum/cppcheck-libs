762df9da218bc7d4ca2ffb3c7874bff7437f0c30
Pavel Roschin <roshin@scriptumplus.ru>
2014-02-09 00:14:04 +0400

    g_idle_add woraround

--- gtk.h
+++ gtk.h
@@ -3,6 +3,11 @@
 #define g_return_val_if_fail(expr, val) do{if(!(expr)){return val;}}while(0)
 #define g_return_if_reached() do{return;}while(0)
 #define g_return_val_if_reached(val) do{return val;}while(0)
+
 /* replace g_list_sort coz it just returns first pointer */
 #define g_list_sort(a, b) a
 #define g_list_sort_with_data(a, b, c) a
+
+/* assume thath g_idle_add is just function call */
+#define g_idle_add(function, data) function(data)
+#define g_idle_add_full (priority, function, data, notify) function(data)
