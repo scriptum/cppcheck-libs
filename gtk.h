6c3e03098a96558cb5b75169c31aa948bcb1782a
Pavel Roschin <roshin@scriptumplus.ru>
2014-02-10 21:42:02 +0400

    revert g_utf8_validate

--- gtk.h
+++ gtk.h
@@ -11,6 +11,3 @@
 /* assume thath g_idle_add is just function call */
 #define g_idle_add(function, data) function(data)
 #define g_idle_add_full(priority, function, data, notify) function(data)
-
-/* ignore write-only value */
-#define g_utf8_validate(str, len, end) end = str, g_utf8_validate(str, len)
